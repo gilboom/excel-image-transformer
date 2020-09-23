fe1ea1efc5e006d416d0975790da3d1e5f7b69f6
gilboom <q86002618@163.com>
2020-09-23 15:10:48 +0800

    init

---
+++ src/excel/excel.ts
@@ -0,0 +1,70 @@
+import * as XLSX from "xlsx";
+import { ExcelImageResolver } from "./image";
+
+export class Excel {
+  workbook: XLSX.WorkBook;
+  imageResolver: ExcelImageResolver;
+
+  load(buffer: Uint8Array | Blob | ArrayBuffer) {
+    this.workbook = XLSX.read(buffer, { type: "buffer", bookFiles: true });
+    console.debug("workbook %o", this.workbook);
+    this.imageResolver = new ExcelImageResolver(this.workbook);
+  }
+
+  async transformImagesToStr(
+    cb: (file: File) => Promise<string>
+  ) {
+    const locationsMap = await this.imageResolver.resolveImageLocations();
+    debugger;
+    for (let sheetName in locationsMap) {
+      const locations = locationsMap[sheetName];
+      const data = this.getDataBySheetName(sheetName);
+      for (const location of locations) {
+        const str = await cb(location.file);
+        data[location.from.row] = data[location.from.row] || [];
+        data[location.from.row][location.from.col] = str;
+      }
+      this.setData(sheetName, data);
+    }
+  }
+
+  getData() {
+    this._assertHasLoaded();
+    return this.workbook.SheetNames.map(name => {
+      return {
+        sheetName: name,
+        data: this.getDataBySheetName(name),
+      }
+    });
+  }
+
+  getDataBySheetName(sheetName: string) {
+    const sheet = this.workbook.Sheets[sheetName];
+    return XLSX.utils.sheet_to_json<Array<any>>(sheet, { header: 1 });
+  }
+
+  setData(sheetName: string, data: Array<Array<any>>) {
+    this._assertSheetExist(sheetName);
+    const sheet = this.workbook.Sheets[sheetName];
+    XLSX.utils.sheet_add_aoa(sheet, data);
+  }
+
+  export() {
+    const workbook = XLSX.utils.book_new();
+    this.workbook.SheetNames.forEach(name => {
+      const sheet = this.workbook.Sheets[name];
+      XLSX.utils.book_append_sheet(workbook, sheet, name);
+    });
+    XLSX.writeFile(workbook, "Sheet.xlsx");
+  }
+
+  private _assertHasLoaded() {
+    if (!this.workbook) throw new Error("还未加载 excel 文件");
+  }
+
+  private _assertSheetExist(sheetName: string) {
+    if (!this.workbook.Sheets[sheetName]) throw new Error(`没有名为 ${sheetName} 的工作薄`);
+  }
+
+}
+
---
+++ src/excel/image.ts
@@ -0,0 +1,169 @@
+import * as XLSX from "xlsx";
+import { xml2js } from "xml-js"
+
+export interface IImageMetadatasMap {
+  [sheetName: string]: Array<IImageMetadata>;
+}
+
+export interface IImageAnchor {
+  from: { col: number, row: number }
+  to: { col: number, row: number }
+}
+
+export interface IImageMetadata extends IImageAnchor {
+  file: File
+}
+
+export type RelationId = string;
+export type SheetName = string;
+export type SheetId = string;
+export type Sheet = {
+  id: SheetId;
+  name: SheetName;
+}
+
+export class ExcelImageResolver {
+  sheetMap: Map<SheetName, Sheet>;
+  constructor(
+    private workbook: XLSX.WorkBook
+  ) { }
+
+  async resolveImageLocations(): Promise<IImageMetadatasMap> {
+    await this._resolveSheetMap();
+    const map = await this._resolveAllSheetImages();
+    console.debug("metadatasMap", map)
+    return map;
+  }
+
+  private async _resolveSheetMap() {
+    const wbXmlObj = await this._parseMetaXmlToObject("xl/workbook.xml");
+    console.debug("wbXmlObj %o", wbXmlObj);
+
+    this.sheetMap = new Map<SheetName, Sheet>(
+      wbXmlObj.workbook.sheets.sheet.map(sheet => {
+        return [sheet.name, { name: sheet.name, id: sheet.sheetId }];
+      })
+    );
+    console.debug("sheetMap %o", this.sheetMap);
+  }
+
+  private async _resolveAllSheetImages(): Promise<IImageMetadatasMap> {
+    const metadatasMap: IImageMetadatasMap = {};
+
+    for (const sheetName of this.workbook.SheetNames) {
+      const sheet = this.sheetMap.get(sheetName);
+      if (!sheet) throw new Error("sheet not found");
+      metadatasMap[sheetName] = await this._resolveSheetImages(sheet.id);
+    }
+    return metadatasMap;
+  }
+
+  private async _resolveSheetImages(sheetId: string): Promise<Array<IImageMetadata>> {
+    const drawingXmlObj = await this._parseMetaXmlToObject(`xl/drawings/drawing${sheetId}.xml`);
+    if (!drawingXmlObj) return [];
+    console.debug("drawingXmlObj", drawingXmlObj);
+
+    const imageAnchors: Array<any> = drawingXmlObj["xdr:wsDr"]["xdr:twoCellAnchor"];
+    const imageAnchorsMap = new Map<RelationId, IImageAnchor>(
+      imageAnchors.map(anchor => {
+        const id = anchor["xdr:pic"]["xdr:blipFill"]["a:blip"]["r:embed"];
+        const from = {
+          row: Number(anchor["xdr:from"]["xdr:row"]),
+          col: Number(anchor["xdr:from"]["xdr:col"])
+        };
+        const to = {
+          row: Number(anchor["xdr:to"]["xdr:row"]),
+          col: Number(anchor["xdr:to"]["xdr:col"]),
+        };
+        return [id, { from, to }];
+      })
+    );
+    console.debug("imageAnchorsMap", imageAnchorsMap);
+
+    const drawingRelXmlObj = await this._parseMetaXmlToObject(`xl/drawings/_rels/drawing${sheetId}.xml.rels`);
+    console.debug("drawingRelXmlObj", drawingRelXmlObj);
+    const relationships: Array<any> = drawingRelXmlObj.Relationships.Relationship;
+
+    const imageMetadatas: Array<IImageMetadata> = relationships.map(r => {
+      const id: RelationId = r.Id;
+      const target: string = r.Target;
+      const filename = target.substr(target.lastIndexOf("/") + 1);
+      const path = `xl/media/${filename}`;
+      const file = this._parseMetaImageToFile(path, filename);
+      const imageAnchor = imageAnchorsMap.get(id);
+      if (!imageAnchor) throw new Error("image anchor not found");
+      const { from, to } = imageAnchor;
+      return { file, from, to };
+    })
+    console.debug("imageMetadatas", imageMetadatas);
+    return imageMetadatas;
+  }
+
+
+  private _getDataFromWbFile(path: string) {
+    const file = this.workbook["files"][path];
+    if (!file) return null;
+
+    const _data: Uint8Array = file._data.getContent ? file._data.getContent() : file._data;
+    return _data;
+  }
+
+  private async _parseMetaXmlToObject<T = any>(path: string) {
+    const _data: Uint8Array | null = this._getDataFromWbFile(path);
+    if (!_data) return null;
+
+    const fileReader = new FileReader();
+    return new Promise<T>((resolve, reject) => {
+      fileReader.onload = (e) => {
+        const xml = e.target.result as string;
+        console.debug(`FileLoader 加载文件事件: `, e)
+        try {
+          resolve(this._xml2Obj<T>(xml));
+        } catch (err) {
+          reject(new Error(`解析 xml 失败 文件名: ${path} 文件内容: ${xml}`))
+        }
+      };
+      fileReader.onerror = (e) => {
+        reject(e.target.error);
+      }
+      fileReader.readAsText(new Blob([_data]));
+    });
+  }
+
+
+  private _parseMetaImageToFile(path: string, filename: string) {
+    const _data: Uint8Array | null = this._getDataFromWbFile(path);
+    if (!_data) return null;
+
+    const file = new File([_data], filename);
+    return file;
+  }
+
+  private _xml2Obj<T>(xml: string): T {
+    const attributesKey = "_attr";
+    const textKey = "_text";
+    const obj = xml2js(xml, { compact: true, alwaysArray: ["xdr:twoCellAnchor", "Relationship"], alwaysChildren: false, attributesKey, textKey }) as T;
+    const newObj: any = {};
+
+    function _mergeAttrAndText(obj: any, newObj: any, newObjKey?: string, newObjParent?: any) {
+      Object.keys(obj)
+        .forEach(k => {
+          const attrs = obj[k]
+          if (k === attributesKey) {
+            Object.assign(newObj, attrs);
+          }
+          else if (k === textKey) {
+            if (newObjParent) newObjParent[newObjKey] = attrs;
+          }
+          else {
+            Object.assign(newObj, { [k]: attrs });
+            if (typeof attrs === "object") {
+              _mergeAttrAndText(attrs, newObj[k], k, newObj);
+            }
+          }
+        })
+    }
+    _mergeAttrAndText(obj, newObj);
+    return newObj;
+  }
+}
